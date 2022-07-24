18de243cb8658dfd1600c888098d98b0c1c5fd44
Shark <rynyaz@gmail.com>
2022-07-25 01:49:36 +0700

    rebuild

--- slider.js
+++ slider.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
+    $('.ads-8 .owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
-        // dots: false,
         autoplay: true,
         autoplayTimeout: 3000,
         autoplaySpeed: 1000,
@@ -16,6 +15,50 @@ $(document).ready(function () {
             },
             1000: {
                 items: 1
+            }
+        }
+    })
+});
+$(document).ready(function () {
+    $('.category-content .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+});
+
+$(document).ready(function () {
+    $('.flashsale-owl .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        dots: false,
+        // autoplay: true,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 6
             }
         }
     })
